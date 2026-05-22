43d91798f267eb37ddfb3092cdf4786bd115a71f
danseoGekijo <dann1601007@gmail.com>
2026-05-22 15:41:43 +0200

    Leagues and cups lojics turned on !

---
+++ assets/competitionsManager/toolsFor/clubsCompetitionsTools.js
@@ -0,0 +1,478 @@
+// by Danseo
+
+// Factory for creating league objects and providing tools to manage them
+
+import { getClubsByContinent, getClubsByCountry } from "../../clubs/clubs.js";
+
+class Club {
+    constructor(id, name, country, continent, specs, stadium) {
+        this.name = name;
+        this.country = country;
+        this.continent = continent;
+        this.stadium = stadium
+        this.specs = specs;
+        this.id = id;
+
+        this.matchesPlayed = 0;
+        this.wins = 0;
+        this.draws = 0;
+        this.losses = 0;
+        this.goalsFor = 0;
+        this.goalsAgainst = 0;
+        this.points = 0;
+    }
+
+    get goalDifference() {
+        return this.goalsFor - this.goalsAgainst;
+    }
+
+    resetStats() {
+        this.matchesPlayed = 0;
+        this.wins = 0;
+        this.draws = 0;
+        this.losses = 0;
+        this.goalsFor = 0;
+        this.goalsAgainst = 0;
+        this.points = 0;
+    }
+
+    recordMatch(goalsFor, goalsAgainst) {
+        this.matchesPlayed += 1;
+        this.goalsFor += goalsFor;
+        this.goalsAgainst += goalsAgainst;
+
+        if (goalsFor > goalsAgainst) {
+            this.wins += 1;
+            this.points += 3;
+        } else if (goalsFor < goalsAgainst) {
+            this.losses += 1;
+        } else {
+            this.draws += 1;
+            this.points += 1;
+        }
+    }
+}
+
+class Match {
+    constructor(homeClub, awayClub) {
+        this.homeClub = homeClub;
+        this.awayClub = awayClub;
+        this.homeGoals = null;
+        this.awayGoals = null;
+        this.played = false;
+    }
+
+    simulateMatch() {
+        const teamA = this.homeClub;
+        const teamB = this.awayClub;
+
+        let teamAScore = 0;
+        let teamBScore = 0;
+
+        let allChances = teamA.specs.reputation + teamB.specs.reputation + 8;
+        let rangeA = Math.ceil(teamA.specs.OVR / 2);
+        let rangeB = Math.floor(teamB.specs.OVR / 2);
+        let rest = 100 - (rangeA + rangeB);
+
+        for (let i = 0; i < allChances; i++) {
+            const random = Math.floor(Math.random() * 100);
+
+            if (random >= 0 && random <= rangeA) {
+                teamAScore += goalAttempt(teamA, teamB)
+            }
+            else if (random > rangeA && random <= rangeA + rangeB) {
+                goalAttemptContest(teamA, teamB);
+            }
+            else {
+                teamBScore += goalAttempt(teamB, teamA);
+            }
+        }
+
+        this.homeGoals = teamAScore;
+        this.awayGoals = teamBScore;
+        this.played = true;
+
+        teamA.recordMatch(teamAScore, teamBScore);
+        teamB.recordMatch(teamBScore, teamAScore);
+
+        function goalAttempt(team, opponent) {
+            const teamAttack = team.specs.attack
+            const teamMidfield = team.specs.midfield
+            const teamDefense = team.specs.defense
+            const opponentAttack = opponent.specs.attack
+            const opponentMidfield = opponent.specs.midfield
+            const opponentDefense = opponent.specs.defense
+
+            let scoreProbability = 20;
+
+            if (teamAttack > opponentDefense) scoreProbability += 5;
+            if (teamMidfield > opponentMidfield) scoreProbability += 3;
+            if (teamDefense > opponentAttack) scoreProbability += 2;
+
+            if (teamAttack === opponentDefense) scoreProbability += 3;
+            if (teamMidfield === opponentMidfield) scoreProbability += 2;
+            if (teamDefense === opponentAttack) scoreProbability += 1;
+
+            if (teamAttack < opponentDefense) scoreProbability -= 6;
+            if (teamMidfield < opponentMidfield) scoreProbability -= 3;
+            if (teamDefense < opponentAttack) scoreProbability -= 3;
+
+            const random = Math.floor(Math.random() * 100);
+
+            return random < scoreProbability ? 1 : 0;
+        }
+
+        function goalAttemptContest(teamA, teamB) {
+            // just a simple randomiser to allow the goalAttempt function to one of 2 teams
+            const random = Math.floor(Math.random() * 100);
+
+            if (random <= 30) {
+                teamAScore += goalAttempt(teamA, teamB);
+            }
+            else if (random > 30 && random <= 70) {
+                return; //
+            }
+            else {
+                teamBScore += goalAttempt(teamB, teamA);
+            }
+        }
+
+
+    }
+
+
+}
+
+class CupMatch extends Match {
+    constructor(homeClub, awayClub) {
+        super(homeClub, awayClub);
+        this.penaltiesWinner = null;
+        this.penaltiesScoreHomeClub = null;
+        this.penaltiesScoreAwayClub = null;
+    }
+    simulateMatch() {
+        const teamA = this.homeClub;
+        const teamB = this.awayClub;
+
+        let teamAScore = 0;
+        let teamBScore = 0;
+
+        let randomFactor = Math.floor(Math.random() * 10) + 1; // Random factor between 1 and 10
+
+        let allChances = randomFactor + 8;
+        let rangeA = Math.ceil(teamA.specs.OVR / 2);
+        let rangeB = Math.floor(teamB.specs.OVR / 2);
+        let rest = 100 - (rangeA + rangeB);
+
+        for (let i = 0; i < allChances; i++) {
+            const random = Math.floor(Math.random() * 100);
+
+            if (random >= 0 && random <= rangeA) {
+                teamAScore += goalAttempt(teamA, teamB)
+            }
+            else if (random > rangeA && random <= rangeA + rangeB) {
+                goalAttemptContest(teamA, teamB);
+            }
+            else {
+                teamBScore += goalAttempt(teamB, teamA);
+            }
+        }
+
+        this.homeGoals = teamAScore;
+        this.awayGoals = teamBScore;
+
+        if (this.homeGoals === this.awayGoals) {
+            this.penaltiesScoreHomeClub = Math.floor(Math.random() * 5) + 3;
+            this.penaltiesScoreAwayClub = Math.floor(Math.random() * 5) + 3;
+            while (this.penaltiesScoreHomeClub === this.penaltiesScoreAwayClub) {
+                this.penaltiesScoreHomeClub = Math.floor(Math.random() * 5) + 3;
+            }
+            this.penaltiesWinner = this.penaltiesScoreHomeClub > this.penaltiesScoreAwayClub ? this.homeClub : this.awayClub;
+        }
+
+        this.played = true;
+
+                function goalAttempt(team, opponent) {
+            const teamAttack = team.specs.attack
+            const teamMidfield = team.specs.midfield
+            const teamDefense = team.specs.defense
+            const opponentAttack = opponent.specs.attack
+            const opponentMidfield = opponent.specs.midfield
+            const opponentDefense = opponent.specs.defense
+
+            let scoreProbability = 20;
+
+            if (teamAttack > opponentDefense) scoreProbability += 5;
+            if (teamMidfield > opponentMidfield) scoreProbability += 3;
+            if (teamDefense > opponentAttack) scoreProbability += 2;
+
+            if (teamAttack === opponentDefense) scoreProbability += 3;
+            if (teamMidfield === opponentMidfield) scoreProbability += 2;
+            if (teamDefense === opponentAttack) scoreProbability += 1;
+
+            if (teamAttack < opponentDefense) scoreProbability -= 6;
+            if (teamMidfield < opponentMidfield) scoreProbability -= 3;
+            if (teamDefense < opponentAttack) scoreProbability -= 3;
+
+            const random = Math.floor(Math.random() * 100);
+
+            return random < scoreProbability ? 1 : 0;
+        }
+
+        function goalAttemptContest(teamA, teamB) {
+            // just a simple randomiser to allow the goalAttempt function to one of 2 teams
+            const random = Math.floor(Math.random() * 100);
+
+            if (random <= 30) {
+                teamAScore += goalAttempt(teamA, teamB);
+            }
+            else if (random > 30 && random <= 70) {
+                return; //
+            }
+            else {
+                teamBScore += goalAttempt(teamB, teamA);
+            }
+        }
+
+
+    }
+
+
+    claimMatchWinner() {
+        if (!this.played) return null;
+
+        if (this.homeGoals > this.awayGoals) {
+            return this.homeClub;
+        } else if (this.homeGoals < this.awayGoals) {
+            return this.awayClub;
+        } else {
+            return this.penaltiesWinner;
+        }
+    }
+
+    textMessageResult() {
+        if (!this.played) return "Match not played yet.";
+
+        return `${this.equipeDomicile.nom} ${this.scoreDomicile}-${this.scoreExterieur} ${this.equipeExterieur.nom} (${this.scoreTabDomicile} - ${this.scoreTabExterieur} 
+                t.a.b, vainqueur: ${this.obtenirVainqueur().nom})`;
+    }
+
+}
+
+class League {
+    constructor(name, country, continent, clubs) {
+        this.name = name;
+        this.country = country;
+        this.continent = continent;
+        this.clubs = clubs
+        this.calendar = [];
+        this.c1Slots = [];
+        this.c2Slots = [];
+
+    }
+
+    generateCalendar() {
+        this.calendar = [];
+        const n = this.clubs.length;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (i !== j) {
+                    this.calendar.push(new Match(this.clubs[i], this.clubs[j]));
+                }
+            }
+        }
+    }
+
+    simulateLeague() {
+        this.generateCalendar();
+        this.calendar.forEach(match => match.simulateMatch());
+        
+
+        // show all matches results
+        this.calendar.forEach(match => {
+             console.log(`${match.homeClub.name} ${match.homeGoals} - ${match.awayGoals} ${match.awayClub.name}`);
+        });
+    }
+
+    claimLeagueRanking() {
+        return [...this.clubs].sort((a, b) => {
+            // Primary: Sort by points (descending)
+            if (b.points !== a.points) {
+                return b.points - a.points;
+            }
+            // Secondary: Sort by goal difference (descending)
+            if (b.goalDifference !== a.goalDifference) {
+                return b.goalDifference - a.goalDifference;
+            }
+            // Tertiary: Sort by goals scored (descending)
+            if (b.goalsFor !== a.goalsFor) {
+                return b.goalsFor - a.goalsFor;
+            }
+            // Final tiebreaker: Sort by wins (descending)
+            return b.wins - a.wins;
+        });
+    }
+}
+
+class NationalCup {
+    constructor(name, country, continent, clubs) {
+        this.name = name;
+        this.country = country;
+        this.continent = continent;
+        this.clubs = clubs;
+        this.calendar = [];
+    }
+
+    drawClubsCup(teams) {
+        return [...teams].sort(() => Math.random() - 0.5);
+    }
+
+    simulateCup(exemptedClubs) {
+        console.log(`Starting ${this.name.toUpperCase()} simulation with ${this.clubs.length} clubs...`);
+
+        const notExemptedClubs = this.clubs.filter(club => !exemptedClubs.includes(club));
+
+        console.log("Round 1");
+
+        const drawRound1 = this.drawClubsCup(notExemptedClubs);
+        const winnersRound1 = [];
+
+        for (let i = 0; i < drawRound1.length; i += 2) {
+            const match = new CupMatch(drawRound1[i], drawRound1[i + 1]);
+            match.simulateMatch();
+            winnersRound1.push(match.claimMatchWinner());
+            // console.log(`Match: ${match.homeClub.name} vs ${match.awayClub.name} - Winner: ${match.claimMatchWinner().name}`);
+        }
+        console.log("Round 1 --- Finished");
+
+        console.log(" Round 2 --- top 10 incomes !!!!!! ");
+        const drawRound2 = this.drawClubsCup([...winnersRound1, ...exemptedClubs]);
+        const winnersRound2 = [];
+
+        for (let i = 0; i < drawRound2.length; i += 2) {
+            const match = new CupMatch(drawRound2[i], drawRound2[i + 1]);
+            match.simulateMatch();
+            winnersRound2.push(match.claimMatchWinner());
+            // console.log(`Match: ${match.homeClub.name} vs ${match.awayClub.name} - Winner: ${match.claimMatchWinner().name}`);
+        }
+        console.log("Round 2 --- Finished");
+
+        console.log("Round of 16");
+        const drawRoundOf16 = this.drawClubsCup(winnersRound2);
+        const winnersRoundOf16 = [];
+
+        for (let i = 0; i < drawRoundOf16.length; i += 2) {
+            const match = new CupMatch(drawRoundOf16[i], drawRoundOf16[i + 1]);
+            match.simulateMatch();
+            winnersRoundOf16.push(match.claimMatchWinner());
+            // console.log(`Match: ${match.homeClub.name} vs ${match.awayClub.name} - Winner: ${match.claimMatchWinner().name}`);
+        }
+        console.log("Round of 16 --- Finished");
+
+        console.log("Quarterfinals");
+        const drawQuarterfinals = this.drawClubsCup(winnersRoundOf16);
+        const winnersQuarterfinals = [];
+
+        for (let i = 0; i < drawQuarterfinals.length; i += 2) {
+            const match = new CupMatch(drawQuarterfinals[i], drawQuarterfinals[i + 1]);
+            match.simulateMatch();
+            winnersQuarterfinals.push(match.claimMatchWinner());
+            // console.log(`Match: ${match.homeClub.name} vs ${match.awayClub.name} - Winner: ${match.claimMatchWinner().name}`);
+        }
+        console.log("Quarterfinals --- Finished");
+
+        console.log("Semifinals");
+        const drawSemifinals = this.drawClubsCup(winnersQuarterfinals);
+        const winnersSemifinals = [];
+
+        for (let i = 0; i < drawSemifinals.length; i += 2) {
+            const match = new CupMatch(drawSemifinals[i], drawSemifinals[i + 1]);
+            match.simulateMatch();
+            winnersSemifinals.push(match.claimMatchWinner());
+            // console.log(`Match: ${match.homeClub.name} vs ${match.awayClub.name} - Winner: ${match.claimMatchWinner().name}`);
+        }
+        console.log("Semifinals --- Finished");
+
+        console.log("Final");
+        const finalMatch = new CupMatch(winnersSemifinals[0], winnersSemifinals[1]);
+        finalMatch.simulateMatch();
+        const cupWinner = finalMatch.claimMatchWinner();
+        console.log(`Final Match: ${finalMatch.homeClub.name} vs ${finalMatch.awayClub.name} - Winner: ${cupWinner.name}`);
+        console.log("Final --- Finished");
+    }
+}
+
+class CountryFederation {
+    constructor(clubs) {
+        this.clubs = clubs;
+        this.mainSeason = 1;
+        this.championsHistory = [];
+        this.cupChampion = [];
+    }
+
+    turnOnTheSeason() {
+        console.log(`\n=============================================`);
+        console.log(`   SEASON N°${this.mainSeason}`);
+        console.log(`=============================================`);
+
+        this.clubs.forEach(club => club.resetStats());
+
+        const clubsD1 = this.clubs.slice(0, 18);
+        const clubsD2 = this.clubs.slice(18, 36);
+        const clubsD3 = this.clubs.slice(36, 54);
+
+        const d1 = new League("Division 1", clubsD1[0].country, clubsD1[0].continent, clubsD1);
+        const d2 = new League("Division 2", clubsD2[0].country, clubsD2[0].continent, clubsD2);
+        const d3 = new League("Division 3", clubsD3[0].country, clubsD3[0].continent, clubsD3);
+
+        d1.simulateLeague();
+        const d1Ranking = d1.claimLeagueRanking();
+        console.log(d1Ranking)
+        this.championsHistory.push(d1Ranking[0].name);
+        console.log(`Champion of ${d1.country} - ${d1.name}: ${d1Ranking[0].name}`);
+
+        d2.simulateLeague();
+        const d2Ranking = d2.claimLeagueRanking();
+        console.log(`Champion of ${d2.country} - ${d2.name}: ${d2Ranking[0].name}`);
+
+        d3.simulateLeague();
+        const d3Ranking = d3.claimLeagueRanking();
+        console.log(`Champion of ${d3.country} - ${d3.name}: ${d3Ranking[0].name}`);
+
+        const exempteds = d1Ranking.slice(0, 10);
+        const nationalCup = new NationalCup("National Cup", d1.country, d1.continent, this.clubs);
+        nationalCup.simulateCup(exempteds);
+
+        this.promotionsManager(d1Ranking, d2Ranking, d3Ranking);
+
+        this.mainSeason++;
+    }
+
+
+    promotionsManager(d1Ranking, d2Ranking, d3Ranking) {
+        // -- division 1 --
+        const d1Saves = d1Ranking.slice(0, 15);
+        const d1Relegated = d1Ranking.slice(15, 18);
+
+        // -- division 2 --
+        const d2Promoted = d2Ranking.slice(0, 3);
+        const d2Saves = d2Ranking.slice(3, 15);
+        const d2Relegated = d2Ranking.slice(15, 18);
+
+        // -- division 3 --
+        const d3Promoted = d3Ranking.slice(0, 3);
+        const d3Saves = d3Ranking.slice(3);
+
+        // -- update clubs for next season --
+        const newD1Clubs = [...d1Saves, ...d2Promoted];
+        const newD2Clubs = [...d2Saves, ...d1Relegated, ...d3Promoted];
+        const newD3Clubs = [...d3Saves, ...d2Relegated];
+
+        this.clubs = [...newD1Clubs, ...newD2Clubs, ...newD3Clubs];
+    }
+}
+
+
+// Exporting the classes for use in other modules
+export { Club, Match, CupMatch, League, NationalCup, CountryFederation };
